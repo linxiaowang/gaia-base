ae76caa8c6dcbe10c5394b2c5d091b2164ac1453
linxiaowang <1104220717@qq.com>
2022-06-24 09:40:49 +0800

    chore: clear files

---
+++ src/components/Header.vue
@@ -0,0 +1,18 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <header w-full p="x-3 y-1" flex="~" items-center justify-between>
+    <div items-center>
+      <div i-carbon-sailboat-offshore />
+    </div>
+    <div items-center>
+      <div i-carbon-sailboat-offshore />
+    </div>
+  </header>
+</template>
+
+<style scoped>
+
+</style>
